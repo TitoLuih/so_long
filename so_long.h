983f9f1233bad224ad1a75bc46b8090e6f151209
Luis Ruiz Torres <lruiz-to@c3r9s5.42malaga.com>
2025-02-07 17:51:22 +0100

    added game struct to .h

---
+++ so_long.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lruiz-to <lruiz-to@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 13:01:17 by lruiz-to          #+#    #+#             */
+/*   Updated: 2025/02/06 16:15:56 by lruiz-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "../libft/libft.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <fcntl.h>
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+typedef struct s_game
+{
+	char			**map;
+	char			**map_copy;
+	int				lines;
+	int				columns;
+	int				c;
+	int				e;
+	int				p;
+	t_point			p_position;
+	int				c_copy;
+	int				e_copy;
+	int				moves;
+	t_point			local_exit;
+	mlx_t			*mlx;
+	mlx_texture_t	*t_coin;
+	mlx_image_t		*i_coin;
+	mlx_texture_t	*t_floor;
+	mlx_image_t		*i_floor;
+	mlx_texture_t	*t_player_r;
+	mlx_image_t		*i_player_r;
+	mlx_texture_t	*t_player_l;
+	mlx_image_t		*i_player_l;
+	mlx_texture_t	*t_open_exit;
+	mlx_image_t		*i_open_exit;
+	mlx_texture_t	*t_closed_exit;
+	mlx_image_t		*i_closed_exit;
+	mlx_texture_t	*t_wall;
+	mlx_image_t		*i_wall;
+}	t_game;
+
+#endif
