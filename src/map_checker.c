63aa54d8303e9118b8f6422e033d24e3d001175b
Luis Ruiz Torres <lruiz-to@c2r2s6.42malaga.com>
2025-02-11 15:48:35 +0100

    read map advanced but not finished

--- src/map_checker.c
+++ src/map_checker.c
@@ -6,7 +6,7 @@
 /*   By: lruiz-to <lruiz-to@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:01:26 by lruiz-to          #+#    #+#             */
-/*   Updated: 2025/02/10 13:11:12 by lruiz-to         ###   ########.fr       */
+/*   Updated: 2025/02/11 13:30:44 by lruiz-to         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,12 @@
 
 int	ext_checker(char *map_name)
 {
-	int i;
-	
+	int	i;
+
 	i = ft_strlen(map_name);
 	if ((map_name[i - 1] == 'r' && map_name[i - 2] == 'e')
 		&& (map_name[i - 3] == 'b' && map_name[i - 4] == '.'))
-		{
-			write(1, "g",1);
-			return (EXIT_SUCCESS);
-		}
+		return (EXIT_SUCCESS);
 	else
 		return (EXIT_FAILURE);
-}
-
-int main (int argc, char **argv)
-{
-	char *map_name;
-
-	if (argc == 2)
-	{
-		ext_checker(&argv[1][0]);
-	}
-	else
-		write (1, "x",1);
 }
--- src/read_map.c
+++ src/read_map.c
@@ -6,51 +6,76 @@
 /*   By: lruiz-to <lruiz-to@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:35:12 by lruiz-to          #+#    #+#             */
-/*   Updated: 2025/02/11 13:00:07 by lruiz-to         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:27:01 by lruiz-to         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void free_map(t_game *game)
+void	free_map(t_game *game)
 {
-	
 }
 
-static int read_column(char *line, t_game *game, int fd, char *map_name)
+static int	read_column(char *line, t_game *game, int fd, char *map_name)
 {
-	
+	int	index;
+
+	index = 0;
+	fd = open(map_name, 0);
+	line = get_next_line(fd);
+	if (index < game->lines)
+	{
+		game->columns = ft_strlen(line);
+		if (line[game->columns - 1] != " \n")
+			game->columns++;
+		game->map[index] = ft_calloc(game->columns, sizeof(char *));
+		game->map_copy[index] = ft_calloc(game->columns, sizeof(char *));
+		if (!game->map || !game->map_copy)
+			return (free(line), EXIT_FAILURE);
+		ft_strlcpy(game->map[index], line, game->columns);
+		ft_strlcpy(game->map_copy[index], line, game->columns);
+		free(line);
+		line = get_next_line(fd);
+		index++;
+	}
+	game->columns--;
+	game->map[index] = NULL;
+	game->map_copy[index] = NULL;
+	return (close(fd), free(line), EXIT_SUCCESS);
 }
 
-static int read_lines(char *line, t_game *game, int fd, char *map_name)
+static int	read_lines(char *line, t_game *game, int fd, char *map_name)
 {
 	fd = open(map_name, 0);
 	line = get_next_line(fd);
 	if (line == NULL)
-		return(ft_printf("Fallo al leer el mapa"), EXIT_FAILURE);
+		return (ft_printf("Fallo al leer el mapa"), EXIT_FAILURE);
 	while (line != NULL)
 	{
 		game->lines++;
 		free(line);
 		line = get_next_line(fd);
 	}
-	
+	game->map = ft_calloc (game->lines + 1, sizeof(char *));
+	game->map_copy = ft_calloc (game->lines + 1, sizeof(char *));
+	if (!game->map || game->map_copy)
+		return (free(line), EXIT_FAILURE);
+	close(fd);
+	if (read_column(line, game, fd, map_name) == 1)
+		return (free(line), EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-int read_map(t_game *game, char *map_name)
+int	read_map(t_game *game, char *map_name)
 {
-	int	fd;
-	char *line;
-	
+	int		fd;
+	char	*line;
+
 	line = NULL;
 	fd = open(map_name, 0);
 	if (fd < 0)
 		return (ft_printf("Error, no se pudo leer el mapa\n"), EXIT_FAILURE);
 	close (fd);
-	
+	if (read_line(line, game, fd, map_name) == 1)
+		return (free(line), EXIT_FAILURE);
 }
-
-/*
-	hay que leer el mapa y copiarlo en map y en map_copy
-	leerlo por lineas y por columna
-*/
